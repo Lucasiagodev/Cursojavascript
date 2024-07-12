bd9f8c71e448180ba99bd902e97bf9d572354f0d
Lucasiagodev <lucasiagodev@gmail.com>
2024-07-12 20:56:25 -0300

    lucas iago

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// memu section
+function menu(){
+    // memu section
 
 document.getElementById('open-nav-menu').addEventListener('click', () => {
     document.querySelector('header nav .wrapper').classList.add('nav-open');
@@ -8,14 +9,22 @@ document.getElementById('close-nav-menu').addEventListener('click', () => {
     document.querySelector('header nav .wrapper').classList.remove('nav-open');
 });
 
+}
 
 // greenting Section
+
+function celsius(temperature){
+    let farh = (temperature * 9/5) + 32;
+    return farh;
+}
 
 const greentingText = "Good morning";
 const weatherCondicion = "Summy"
 const userlocation = "New york"
-let temperature = 22.4556
-let westherText = `The weather is ${weatherCondicion} in ${userlocation} and it s´ ${temperature.toFixed(1)}`;
+let temperature = 45;
+let westherText = `The weather is ${weatherCondicion} in ${userlocation} and it s´ ${celsius(temperature).toFixed(1)}`;
 
 document.querySelector('#greeting').innerHTML = greentingText;
 document.querySelector('p#weather').innerHTML = westherText;
+
+menu()
