0e4a5cdbdf9f615e4093477207056c60e20e90a3
Lucasiagodev <lucasiagodev@gmail.com>
2024-07-14 19:10:11 -0300

    lucas iago

--- main.js
+++ main.js
@@ -9,14 +9,58 @@ function menu() {
         document.querySelector('header nav .wrapper').classList.remove('nav-open');
     });
 }
+setInterval(function timer(){
+    const horas = document.querySelectorAll('.time-number');
+    
+    const data = new Date();
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+    let segundo = data.getSeconds();
+    // use if quando for usar queryseletorAll para adicionar 
+    //as horas nos seus indices com as variaveis criadas
+    if(horas.length === 3){
+
+        //SEGUNDO METÓDO DE HORAS PARA MOSTRAR NO HTML
+            // usar String para converter a variavel number, e padStart() 
+            //para adicionar um caractere antes e padEnde() para adicionar um caractere depois.
+        horas[0].textContent = String(hora).padStart(2,'0');
+        horas[1].textContent = String(minuto).padStart(2,'0');
+        horas[2].textContent = String(segundo).padStart(2,'0');
+        
+        // o forEach colocar os estilos para cada elemento
+        // horas.forEach(item =>{
+        //     item.style.color = "yellow"
+        // });
+
+
+        // PRIMEIRO METÓDO DE HORAS PAR AMOSTRAR NO HTML
+
+        // horas[0].textContent = hora < 10 ? '0' + hora : hora;
+        // horas[1].textContent = minuto < 10 ? '0' + minuto : minuto;
+        // horas[2].textContent = segundo < 10 ? '0' + segundo : segundo;
+           
+    }
+        
+            
+}, 1000);
 
 function temperatura() {
+   let greetingText = "Good morning";
+    let setHoras = new Date().getHours();
+    if(setHoras < 18){
+        greetingText = "Good morning";
+    }else if(setHoras < 19 ){
+         greetingText = "Good afternoon";
+    }else if(setHoras){
+        greetingText = "Good evening"
+}
+   
     function farhCelsius(temperature) {
         let farh = (temperature * 9 / 5) + 32;
         return farh;
     }
 
-    const greetingText = "Good morning";
+    
     const weatherCondition = "Sunny";
     const userLocation = "New York";
     let temperature = 45;
@@ -34,6 +78,7 @@ function temperatura() {
             document.querySelector('#weather').innerHTML = fahrText;
         }
     });
+
 }
 
 function tubnails(){
